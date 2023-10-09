1ea2a0872af1be49e7c9cf85682f5c0505cdf207
Shahadat Hossain Shamim <shamim131666@gmail.com>
2023-10-10 01:11:08 +0600

    Storage & Recently Updated Section

--- lib/pages/team_folder.dart
+++ lib/pages/team_folder.dart
@@ -8,8 +8,12 @@ class TeamFolderPage extends StatefulWidget {
 }
 
 class _TeamFolderPageState extends State<TeamFolderPage> {
+
+  double availableScreenWidth = 0;
+
   @override
   Widget build(BuildContext context) {
+    availableScreenWidth = MediaQuery.of(context).size.width - 50;
     return Scaffold(
       backgroundColor: Colors.grey[100],
       body: Column(
@@ -131,9 +135,112 @@ class _TeamFolderPageState extends State<TeamFolderPage> {
             height: 25,
           ),
 
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25,),
+            child: Row(children: [
+              buildFileSizeChart("SOURCES", Colors.blue, .3),
+              const SizedBox(width: 2,),
+              buildFileSizeChart("DOCS", Colors.red, .25),
+              const SizedBox(width: 2,),
+              buildFileSizeChart("IMAGES", Colors.yellow, .20),
+              const SizedBox(width: 2,),
+              buildFileSizeChart("", Colors.grey, .23),
+            ],
+            ),
+          ),
+          const SizedBox(height: 15,),
+          const Divider(height: 20,),
+          
+          Expanded(
+            child: ListView(
+              padding: EdgeInsets.all(25),
+              children: [
+                const Text(
+                  "Recently updated",
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
 
+                Row(
+                  children: [
+                    buildFileColumn('monitor', 'desktop','.sketch'),
+                    SizedBox(width: availableScreenWidth * .03,),
+                    buildFileColumn('smartphone', 'mobile','.sketch'),
+                    SizedBox(width: availableScreenWidth * .03,),
+                    buildFileColumn('interactive', 'interactive','.prd'),
+                  ],
+                ),
+                Divider(height: 60,),
+              ],
+            ),
+          )
         ],
       ),
     );
+  }
+
+  Column buildFileColumn(String image, String filename, String extension) {
+    return Column(
+                    children: [
+                      Container(
+                        width: availableScreenWidth * .31,
+                        decoration: BoxDecoration(
+                          color: Colors.grey.shade200,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        padding: EdgeInsets.all(30),
+                        height: 110,
+                        child: Image.asset('lib/images/$image.png'),
+                      ),
+                      SizedBox(height: 20,),
+                      RichText(text:
+                          TextSpan(
+                            text: filename,
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 14,
+                            ),
+                            children: [
+                              TextSpan(
+                                text: extension,
+                                style: TextStyle(
+                                  color: Colors.grey,
+                                  fontWeight: FontWeight.w300,
+                                  fontSize: 12,
+                                ),
+                              ),
+                            ],
+                          ),
+                      ),
+                    ],
+                  );
+  }
+
+  Column buildFileSizeChart(String title, Color color, double widthPercentage) {
+    return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  width: availableScreenWidth * widthPercentage,
+                  height: 4,
+                  color: color,
+                ),
+                SizedBox(
+                  height: 8,
+                ),
+                Text(
+                  title,
+                  style: TextStyle(
+                    fontSize: 10,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            );
   }
 }
